de98e2fba70a69b18282851fea6bec3347a19e08
EX867 <karnosex867@gmail.com>
2017-10-25 01:23:14 +0900

    미완성. 파서.java에서 커맨드 형식을 뽑아서 형식의 이름을 반환하는 함수를 만들어야함

--- _library/CommandScript/src/com/karnos/commandscript/Parser.java
+++ _library/CommandScript/src/com/karnos/commandscript/Parser.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 public class Parser {
   MergingTree commands;
-  HashMap<String,String> commandNames;//just giving a name for parameter sequence!
+  HashMap<String, String> commandNames;//just giving a name for parameter sequence!
   LineCommandProcesser processer;
   public Multiset<LineError> errors;
   private LineError cacheError;
@@ -12,7 +12,7 @@ public class Parser {
   public char wrapper='\"';
   public Parser(LineCommandProcesser processer_) {
     errors=new Multiset<LineError>();
-    commandNames=new HashMap<String,String>();
+    commandNames=new HashMap<String, String>();
     cacheError=new LineError(LineError.PRIOR, 0, "", "");
     processer=processer_;
   }
@@ -31,54 +31,54 @@ public class Parser {
     ArrayList<String> params=new ArrayList<String>();
     int a=0;
     StringBuilder buffer=new StringBuilder();
-    while(a<text.length()){
-      while(text.charAt(a)!=seperator){
+    while (a < text.length()) {
+      while (text.charAt(a) != seperator) {
         buffer.append(text.charAt(a));
         a++;
       }
       params.add(buffer.toString());
-      addToKey(key,buffer.toString());
+      addToKey(key, buffer.toString());
       buffer=new StringBuilder();
       a++;//skip the seperator.
     }
-    if(buffer.length()!=0){
+    if (buffer.length() != 0) {
       params.add(buffer.toString());
-      addToKey(key,buffer.toString());
+      addToKey(key, buffer.toString());
     }
-    return processer.buildCommand(commandNames.get(key.toString().trim()),params);
+    return processer.buildCommand(commandNames.get(key.toString().trim()), params);
   }
-  void addToKey(StringBuilder base,String token){
-    if(token)
+  void addToKey(StringBuilder base, String token) {
+    if (token)
   }
-  static boolean isBoolean (String in) {
-    if (in.equals ("true")||in.equals("false"))return true;
+  private static boolean isBoolean(String in) {
+    if (in.equals("true") || in.equals("false")) return true;
     return false;
   }
-  public static boolean isInt(String str) {
+  private static boolean isInt(String str) {
     if (str.equals("")) return false;
     if (str.length() > 9) return false;
-    if (str.equals("-"))return false;
+    if (str.equals("-")) return false;
     // just int or float is needed!
-    int a = 0;
-    if (str.charAt(0) == '-') a = 1;
+    int a=0;
+    if (str.charAt(0) == '-') a=1;
     while (a < str.length()) {
-      if (!('0' <= str.charAt(a) && str.charAt(a) <='9'))return false;
-      a = a + 1;
+      if (!('0' <= str.charAt(a) && str.charAt(a) <= '9')) return false;
+      a=a + 1;
     }
     return true;
   }
-  private boolean isRange(String str) {
-    if (isInt(str))return true;
+  private static boolean isRange(String str) {
+    if (isInt(str)) return true;
     String[] ints=str.split("~");
-    return isInt(ints[0])&&isInt(ints[1]);
+    return isInt(ints[0]) && isInt(ints[1]);
   }
-  private int getRangeFirst(String str) {
-    if (isInt(str))return Integer.parseInt(str);
+  private static int getRangeFirst(String str) {
+    if (isInt(str)) return Integer.parseInt(str);
     String[] ints=str.split("~");
     return Integer.parseInt(ints[0]);
   }
-  private int getRangeSecond(String str) {
-    if (isInt(str))return Integer.parseInt(str);
+  private static int getRangeSecond(String str) {
+    if (isInt(str)) return Integer.parseInt(str);
     String[] ints=str.split("~");
     return Integer.parseInt(ints[1]);
   }
